00f29a6749d11ad22a42fdc8d9cb607961e05f4d
Todd Briley <todd.briley@outlook.com>
2017-02-13 23:33:21 -0500

    added more words

--- hangman.py
+++ hangman.py
@@ -3,21 +3,22 @@ def print_hangman():
     print "  ------"
     print "  |    |"
     print "  |    " + ("O" if (guesses<7) else " ")
-    print "  |   " + ("\\" if (guesses <5) else " ") + ("|" if (guesses<6) else " ") + ("/" if (guesses<4) else " ")
+    print "  |   " + ("\\" if (guesses <5) else " ") + ("|" if (guesses<6 )else " ") + ("/" if (guesses<4) else " ")
     print "  |    " + ("|" if (guesses <3) else " ")
     print "  |   " + ("/" if (guesses <2) else " ") + " " + ("\\" if (guesses<1) else " ")
     print " ---"
     print "--------------------------------"
 
-words =["account","address","advance","auction","brother",
+words =["account","address","advance","computer","apple,","hangman","banana","kitchen", "clock","house","auction","brother",
         "cabinet","captain","holding","install","instant","visible","advisor",
         "aborted","bracket","builder","crimson","custard","fixture"]
+
 word = words[int(random.randint(0,len(words)-1))]
 original_word=word
 # print "the word is:", word
-guesses = 7
+guesses=7
 strguesses = ""
-wip = "_ _ _ _ _ _ _ "
+wip = "_ "* len(original_word)
 winner =0
 print "Welcome to Hangman. I have a word can you guess it?"
 while guesses>0:
@@ -32,7 +33,7 @@ while guesses>0:
             found = word.find(guess)
             word = word[:found] + "*" + word[found+1:]
             wip = wip[:found*2] + guess + wip[found*2+1:]
-        if (word == "*"*7):
+        if (word == "*"*len(original_word)):
             guesses=0
             winner = 1
         if not winner:
